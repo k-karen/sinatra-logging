b0f1a9fb08c996aa600c08e73a3af6933737c248
k-karen <k-karen@users.noreply.github.com>
2023-07-21 21:31:15 +0900

    add cors

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to run
+
+```
+bundle
+bundle exec ruby sinatra.rb
+ngrok http 4567
+```
--- sinatra.rb
+++ sinatra.rb
@@ -1,15 +1,26 @@
 require 'sinatra'
 require 'json'
 
+require 'rack/cors'
+
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+  end
+end
+
 post '/debug' do
   request.body.rewind
   data = JSON.parse request.body.read
+  ts = Time.now
   
   # Append data to log
   File.open('log/server.log', 'a+') { |f|
-    f.puts "#### #{Time.now} ####"
-    f.puts data
+    f.puts "#### #{ts} ####"
+    f.puts data.to_json
   }
+  File.open("log/#{ts.to_i}-#{ts.usec}.json", 'a+') { _1.puts JSON.pretty_generate(data) }
 
   status 200
 end
